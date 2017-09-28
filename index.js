0e532bc7d723b7d5a7b10964f74e1c9516082543
Chloe Hwang <c.chloeh@gmail.com>
2017-09-28 16:11:56 -0400

    set up external stylesheet

--- index.js
+++ index.js
@@ -68,11 +68,12 @@ window.explorationsNav = {
   },
 
   generateStylesheet: function() {
-    let css = `.gnav-explorations-nav__button__content,.gnav-explorations-nav__top .gnav-explorations-nav__container{display:flex}.gnav-explorations-nav *{font:inherit;-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;letter-spacing:1px}.gnav-explorations-nav__top{background-color:${this.colorBase}}.gnav-explorations-nav__logo{background-color:${this.colorLogo}}.gnav-explorations-nav__bottom{background-color:${this.colorMenu}}.gnav-explorations-nav__link.-current{opacity:.5}.gnav-explorations-nav{height:45px;width:100%;z-index:100;color:#fff}.gnav-explorations-nav.-open{height:auto}.gnav-explorations-nav.-open .gnav-explorations-nav__bottom{transform:translateY(0);transition:transform .3s ease-in-out}.gnav-explorations-nav__container{margin:auto;max-width:374px;height:inherit}.gnav-explorations-nav__container ul{list-style:none;margin:0}.gnav-explorations-nav__top{position:relative;z-index:10}.gnav-explorations-nav__logo{padding:11px 24px 6px;height:inherit}.gnav-explorations-nav__logo svg{height:23px;opacity:.8;width:68px}.gnav-explorations-nav__button{box-sizing:border-box;background:0 0;border:0;color:inherit;font:inherit;font-size:17px;overflow:visible;padding:0;-webkit-appearance:none;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;cursor:pointer;font-weight:700;margin:0 auto;opacity:.8;text-align:center;text-transform:uppercase}.gnav-explorations-nav__button__svg{fill:#fff;height:15px;margin-left:10px;width:15px}.gnav-explorations-nav__bottom{padding:13px;position:relative;transform:translateY(-100%);transition:transform .3s ease-in-out;z-index:2}.gnav-explorations-nav__link{color:inherit;display:block;font-weight:700;padding:5px 13px}.gnav-explorations-nav a{position:static;text-transform:uppercase}.gnav-explorations-nav a:after,.gnav-explorations-nav a:before{display:none;position:static}@media screen and (min-width:990px){.gnav-explorations-nav{position:absolute;right:0;top:0;width:374px}}`;
+    let css = `.gnav-explorations-nav__top{background-color:${this.colorBase}}.gnav-explorations-nav__logo{background-color:${this.colorLogo}}.gnav-explorations-nav__bottom{background-color:${this.colorMenu}}`;
 
-    let stylesheet = document.createElement('style');
+    let stylesheet = document.createElement('link');
     stylesheet.type = 'text/css';
-    stylesheet.innerHTML = css;
+    stylesheet.rel = 'stylesheet';
+    stylesheet.href = './style.css';
 
     return stylesheet;
   }
